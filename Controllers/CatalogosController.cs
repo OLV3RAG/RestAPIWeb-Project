1e74ad89ba84b0f63fa99a9ccceda2cb24a4dccc
OLV3RAG <daaaayv@gmail.com>
2025-12-22 15:16:28 -0600

    Cambios en el codigo

---
+++ Controllers/CatalogosController.cs
@@ -0,0 +1,57 @@
+using Aplicacion;
+using Infraestructura.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestAPIWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogosController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<CatalogosController> _logger;
+
+        private IConfiguration configuration;
+
+        public CatalogosController(ILogger<CatalogosController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            this.configuration = configuration;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("ObtenerMaterias")]
+        public List<Materias> ObtenerMaterias()
+        {
+            List<Materias> listaMaterias = new List<Materias>();
+            CatalogoNegocio catNeg = new CatalogoNegocio(configuration);
+            listaMaterias = catNeg.ObtenerCatalogoMat();
+            return listaMaterias;
+        }
+        [HttpGet("ObtenerGenerosAlumnos")]
+        public List<Genero> ObtenerGenerosdeAlumnos()
+        {
+            List<Genero> gen = new List<Genero>();
+            CatalogoNegocio catNeg = new CatalogoNegocio(configuration);
+            gen = catNeg.ObtenerCatalogoGen();
+            return gen;
+            
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddScoped <AccesoBD>();
+
+// builder para cargar configuracion de conexion
+builder.Services.AddSingleton<BaseDbConfig>(new BaseDbConfig(builder.Configuration));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
