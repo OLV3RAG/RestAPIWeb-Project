71c155170d82e8e09aa747820a9163d5467c344a
OLV3RAG <daaaayv@gmail.com>
2025-12-26 15:32:25 -0600

    Cambios en el codigo

---
+++ Aplicacion/CatalogosNegocio.cs
@@ -0,0 +1,39 @@
+﻿using Infraestructura;
+using Infraestructura.Entidades;
+using Infraestructura.Generos;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+namespace Aplicacion
+{
+    public class CatalogoNegocio
+    {
+        private readonly IConfiguration _configuration;
+
+        public CatalogoNegocio(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public List<Genero> ObtenerCatalogoGen()
+        {
+            List<Genero> gene = new List<Genero>();
+            GenerosDAO genDao = new GenerosDAO(_configuration);
+            gene = genDao.ObtenerGenero();
+            return gene;
+        }
+        public List<Materias> ObtenerCatalogoMat()
+        {
+            List<Materias> listaMaterias = new List<Materias>();
+            MateriasDAO matDao = new MateriasDAO(_configuration);
+           listaMaterias = matDao.ObtenerMaterias();
+            return listaMaterias;
+        }
+        public List<TipoPersona> ObtenerCatalogoTipPer()
+        {
+            List<TipoPersona> tipPer = new List<TipoPersona>();
+            TipoPersonasDAO tipPerDao = new TipoPersonasDAO(_configuration);
+            tipPer = tipPerDao.ObtenerTipoPersona();
+            return tipPer;
+        }
+    }
+}
